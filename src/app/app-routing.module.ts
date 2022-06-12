e2c13ecad607005a7178c750043f8733fe419f8a
Jeffrey Saelee <gwfds123@gmail.com>
2022-06-12 16:54:12 -0700

    Changed login routing and added css changes to landing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
 ];
 
