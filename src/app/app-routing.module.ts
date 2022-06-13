875462ea9fe14ac49566d88514d8db8d6432fb84
alezubi <alejandro.zubillaga@hotmail.com>
2022-06-13 16:45:47 -0400

    Profile added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'register', component: RegisterComponent},
+  { path: 'profile', component: ProfileComponent}
+
 ];
 
 @NgModule({
