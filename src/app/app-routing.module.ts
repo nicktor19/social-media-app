42ea2d1f721268c82b4744867994fc9fce87fc5a
Jeffrey Saelee <gwfds123@gmail.com>
2022-06-13 18:42:17 -0700

    changed login/out navbar icons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 
